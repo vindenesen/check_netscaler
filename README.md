c95437c6a413fa511e2df374d0611c0267de29fd
Simon Lauger <simon@lauger.name>
2015-12-17 20:47:46 +0100

    updated README.md (usage examples)

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A Nagios Plugin written in Perl for the Citrix NetScaler. It's based on the the 
 
 On a CentOS machine execute the following commands to install all dependencies (LWP, JSON):
 
-    yum install perl-libwww-perl perl-JSON
+    yum install perl-libwww-perl perl-JSON perl-Nagios-Plugin
 
 # Usage Examples
 
--- check_netscaler.pl
+++ check_netscaler.pl
@@ -6,35 +6,30 @@
 # Simon Lauger <simon@lauger.name>
 #
 # https://github.com/slauger/check_netscaler
-# http://blog.simlau.net/
 #
-# Copyright (c) 2015, Simon Lauger
-# All rights reserved.
+# Copyright (c) 2015-2016 Simon Lauger <simon.lauger@teamix.de>
 # 
 # Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions are met:
+# modification, are permitted provided that the following conditions
+# are met:
 # 1. Redistributions of source code must retain the above copyright
 #    notice, this list of conditions and the following disclaimer.
 # 2. Redistributions in binary form must reproduce the above copyright
 #    notice, this list of conditions and the following disclaimer in the
 #    documentation and/or other materials provided with the distribution.
-# 3. All advertising materials mentioning features or use of this software
-#    must display the following acknowledgement:
-#    This product includes software developed by the <organization>.
-# 4. Neither the name of the <organization> nor the
-#   names of its contributors may be used to endorse or promote products
-#   derived from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
-# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
-# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# 3. The name of the author may not be used to endorse or promote products
+#    derived from this software without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+# IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+# NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+# THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+# THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 ################################################################################
 
 use Nitro;
@@ -70,8 +65,7 @@ Perl NITRO API samples)
 
 This Plugin is written and maintained by Simon Lauger.
 
-GitHub: https://github.com/slauger/check_netscaler
-Offical Blog: http://blog.simlau.net
+https://github.com/slauger/check_netscaler
 
 Usage Examples:
 
@@ -156,6 +150,10 @@ foreach my $arg (@args) {
 }
 
 $plugin->getopts;
+
+if (!defined $plugin->opts->hostname) {
+	$plugin->nagios_die('missing hostname argument', CRITICAL);
+}
  
 my $session = Nitro::_login($plugin->opts->hostname, $plugin->opts->username, $plugin->opts->password);
 
