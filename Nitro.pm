60e36fb5c003787aff93dd7c158faba0fae167af
root <root@web01.linux-dev.de>
2015-12-17 20:19:47 +0100

    added check_netscaler.pl

---
+++ Nitro.pm
@@ -0,0 +1,306 @@
+=begin comment
+* Copyright (c) 2008-2015 Citrix Systems, Inc.
+*
+*   Licensed under the Apache License, Version 2.0 (the "License");
+*   you may not use this file except in compliance with the License.
+*   You may obtain a copy of the License at
+*
+*       http://www.apache.org/licenses/LICENSE-2.0
+*
+*  Unless required by applicable law or agreed to in writing, software
+*   distributed under the License is distributed on an "AS IS" BASIS,
+*   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+*   See the License for the specific language governing permissions and
+*   limitations under the License.
+=end comment
+=cut
+
+package Nitro;
+
+use strict;
+use LWP;
+use Carp;
+use JSON;
+use URI::Escape;
+
+# Login method : Used to login to netscaler and get session
+# Arguments : ipaddress, username, password (of netscaler)
+sub _login {
+
+	my ($ipaddress,$username,$password) = @_ ;
+
+	if (!$ipaddress || $ipaddress eq "") {
+		Carp::confess "Error : IP Address should not be null";
+	}
+	if (!$username || $username eq "") {
+		Carp::confess "Error : Username should not be null";
+	}
+	if (!$password || $password eq "") {
+		Carp::confess "Error : Password should not be null";
+	}
+	my $obj = undef;
+        $obj->{username} = $username;
+	$obj->{password} = $password;
+	my $payload = JSON->new->allow_blessed->convert_blessed->encode($obj);
+	$payload = '{"login" :'.$payload."}";
+
+	my $url = "http://$ipaddress/nitro/v1/config/login";
+	my $contenttype = "application/vnd.com.citrix.netscaler.login+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new( POST => $url );
+	$request->header( 'Content-Type', $contenttype );
+	$request->content($payload);
+
+	my $response = $nitro_useragent->request($request);
+	my $session = undef;
+	if (HTTP::Status::is_error($response->code)) {
+		$session = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	} else {
+		my $cookie = $response->header('Set-Cookie');
+		if ($cookie && $cookie =~ /NITRO_AUTH_TOKEN=(.*);/) {
+			$session->{sessionid} = uri_unescape($1);
+		}
+		$session->{errorcode} = 0;
+		$session->{message} = "Done";
+	}
+	$session->{ns} = $ipaddress;
+	$session->{username} = $username;
+	$session->{password} = $password;
+	return $session;
+}
+
+# POST method : Used to clear, enable, add, unset, bind, import, export and save the configuration
+# Arguments : session, objecttype, object, operation
+sub _post {
+
+	my ($session, $objecttype, $object, $operation) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+	if (!$objecttype || $objecttype eq "") {
+		Carp::confess "Error : Object type should not be null";
+	}
+	if (!$object || $object eq "") {
+		Carp::confess "Error : Object should not be null";
+	}
+
+    my $payload = JSON->new->allow_blessed->convert_blessed->encode($object);
+    $payload = '{"'.$objecttype.'" :'.$payload."}";
+
+	my $url = "http://$session->{ns}/nitro/v1/config/".$objecttype;
+	if ($operation && $operation ne "add") {
+		$url  = $url. "?action=".$operation;
+	}
+	my $contenttype = "application/vnd.com.citrix.netscaler.".$objecttype."+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(POST => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+	$request->content($payload);
+
+	my $response = $nitro_useragent->request($request);
+	if (HTTP::Status::is_error($response->code)) {
+		$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	} else {
+		$response->{errorcode} = 0;
+		$response->{message} = "Done";
+	}
+	return $response;
+}
+
+# GET method : Used to get the details of configuration
+# Arguments : session, objecttype, objectname, options
+sub _get {
+
+	my ($session, $objecttype, $objectname, $options) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+	if (!$objecttype || $objecttype eq "") {
+		Carp::confess "Error : Object type should not be null";
+	}
+
+	my $url = "http://$session->{ns}/nitro/v1/config/".$objecttype;
+	if ($objectname && $objectname ne "") {
+		$url  = $url."/".uri_escape(uri_escape($objectname));
+	}
+	if ($options && $options ne "") {
+		$url = $url."?".$options;
+	}
+	my $contenttype = "application/vnd.com.citrix.netscaler.".$objecttype."+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(GET => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+
+	my $response = $nitro_useragent->request($request);
+	$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	return $response;
+}
+
+# Get stats method : Used to get the stats of the configuration
+# Arguments : session, objecttype, objectname
+sub _get_stats {
+
+	my ($session, $objecttype, $objectname) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+	if (!$objecttype || $objecttype eq "") {
+		Carp::confess "Error : Object type should not be null";
+	}
+
+	my $url = "http://$session->{ns}/nitro/v1/stat/".$objecttype;
+	if ($objectname && $objectname ne "") {
+		$url  = $url. "/".uri_escape(uri_escape($objectname));
+	}
+	my $contenttype = "application/vnd.com.citrix.netscaler.".$objecttype."+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(GET => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+
+	my $response = $nitro_useragent->request($request);
+	$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	return $response;
+}
+
+# PUT method : Used to update the already existing configuration
+# Arguments : session, objecttype, object, objectname
+sub _put
+{
+	my ($session, $objecttype, $object, $objectname) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+	if (!$objecttype || $objecttype eq "") {
+		Carp::confess "Error : Object type should not be null";
+	}
+	if (!$object || $object eq "") {
+		Carp::confess "Error : Object should not be null";
+	}
+
+	my $payload = JSON->new->allow_blessed->convert_blessed->encode($object);
+        $payload = '{"'.$objecttype.'" :'.$payload."}";
+
+	my $url = "http://$session->{ns}/nitro/v1/config/".$objecttype. "/".uri_escape(uri_escape($objectname));
+	my $contenttype = "application/vnd.com.citrix.netscaler.".$objecttype."+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(PUT => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+	$request->content($payload);
+
+	my $response = $nitro_useragent->request($request);
+	if (HTTP::Status::is_error($response->code)) {
+		$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	} else {
+		$response->{errorcode} = 0;
+		$response->{message} = "Done";
+	}
+	return $response;
+}
+
+# DELETE method : Used to delete, unbind the existing configuration
+# Arguments : session, objecttype, object
+sub _delete {
+
+	my ($session, $objecttype, $object) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+	if (!$objecttype || $objecttype eq "") {
+		Carp::confess "Error : Object type should not be null";
+	}
+	if (!$object || $object eq "") {
+		Carp::confess "Error : Object should not be null";
+	}
+
+        my $url = "http://$session->{ns}/nitro/v1/config/$objecttype";
+	if (ref($object) eq 'HASH') {
+		$url = $url."?args=";
+		while ((my $key, my $value) = each %{$object}) {
+			$url = $url.$key.":".uri_escape(uri_escape($value)).",";
+		}
+		$url =~ s/,$//;
+	} else {
+		$url  = $url."/".uri_escape(uri_escape($object));
+	}
+	my $contenttype = "application/vnd.com.citrix.netscaler.".$objecttype."+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(DELETE => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+
+	my $response = $nitro_useragent->request($request);
+	if (HTTP::Status::is_error($response->code)) {
+		$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	} else {
+		$response->{errorcode} = 0;
+		$response->{message} = "Done";
+	}
+	return $response;
+}
+
+# Logout method : Used to logout the netscaler
+# Arguments : session
+sub _logout {
+
+	my ($session) = @_ ;
+
+	if (!$session || $session eq "") {
+		Carp::confess "Error : Session should not be null";
+	}
+	if (!($session->{sessionid})) {
+		Carp::confess "Error : Not logged in";
+	}
+
+	my $payload = '{"logout" :{}}';
+
+	my $url = "http://$session->{ns}/nitro/v1/config/logout";
+	my $contenttype = "application/vnd.com.citrix.netscaler.logout+json";
+
+	my $nitro_useragent = LWP::UserAgent->new;
+	my $request = HTTP::Request->new(POST => $url);
+	$request->header('Content-Type', $contenttype);
+	$request->header('Set-Cookie', "NITRO_AUTH_TOKEN=".$session->{sessionid});
+	$request->content($payload);
+
+	my $response = $nitro_useragent->request($request);
+	if (HTTP::Status::is_error($response->code)) {
+		$response = JSON->new->allow_blessed->convert_blessed->decode($response->content);
+	} else {
+		$response->{errorcode} = 0;
+		$response->{message} = "Done";
+	}
+	return $response;
+}
+
+1;
+
---
+++ check_netscaler.pl
@@ -0,0 +1,423 @@
+#!/usr/bin/perl -w
+################################################################################
+# check_netscaler
+# 
+# Nagios Check Script Citrix NetScaler 
+# Simon Lauger <simon@lauger.name>
+#
+# http://blog.simlau.net
+# https://github.com/slauger/check_netscaler
+#
+# Copyright (c) 2015, Simon Lauger
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+# 1. Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer.
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+# 3. All advertising materials mentioning features or use of this software
+#    must display the following acknowledgement:
+#    This product includes software developed by the <organization>.
+# 4. Neither the name of the <organization> nor the
+#   names of its contributors may be used to endorse or promote products
+#   derived from this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ''AS IS'' AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL <COPYRIGHT HOLDER> BE LIABLE FOR ANY
+# DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+# ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+# SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+################################################################################
+
+use Nitro;
+use Nagios::Plugin;
+use Data::Dumper;
+
+use strict;
+
+my $plugin = Nagios::Plugin->new(
+	plugin		=> 'check_netscaler',
+	shortname	=> 'check_netscaler',
+	version		=> '0.1',
+	url		=> 'https://github.com/slauger/check_netscaler',
+	blurb		=> 'Nagios Plugin for Citrix NetScaler Appliance (VPX/MPX/SDX)',
+	usage		=> "Usage: %s [ -v|--verbose ] [ -H <host> ] [ -U <username> ] [ -P <password> ] [ -t <timeout> ] -H <host> -C <command> -I <identifier> -F <filter>",
+	license		=>
+"This nagios plugin is free software, and comes with ABSOLUTELY NO WARRANTY.
+It may be used, redistributed and/or modified under the terms of the 3-Clause
+BSD License (see http://opensource.org/licenses/BSD-3-Clause).",
+ 	extra     => "
+This plugin connects to a Citrix NetScaler appliance trough the NITRO API and checks 
+different parameters regarding vservers, servicegroups, services, loadbalancing, 
+content switching, aaa and vpn. The goal of this project is to have one plugin for 
+every important metric on the Citrix NetSaler.
+
+This plugin also works for MPX/SDX NetScaler Appliances.
+
+Performance data and grahing will be shipped in a later version of this plugin.
+
+The Nitro.pm by Citrix (releases under the Apache License 2.0) is required for using this 
+plugin. Use the Download tab in your NetScaler WebGUI for getting the Nitro.pm (part of the
+Perl NITRO API samples)
+
+This Plugin is written and maintained by Simon Lauger.
+
+GitHub: https://github.com/slauger/check_netscaler
+
+Offical Blog: http://blog.simlau.net
+
+Usage Examples:
+
+NetScaler::VPNvServer::State 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_vserver -I vpnvserver 
+
+NetScaler::LBvServer::State 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot-C check_vserver -I lbvserver 
+
+NetScaler::System::Memory 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_threshold_above -I system -F memusagepcnt -w 75 -c 80 
+
+NetScaler::System::CPU 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_threshold_above -I system -F cpuusagepcnt -w 75 -c 80 
+
+NetScaler::System::CPU::MGMT 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_threshold_above -I system -F mgmtcpuusagepcnt -w 75 -c 80 
+
+NetScaler::System::Disk0 
+/check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_threshold_above -I system -F disk0perusage -w 75 -c 80 
+
+NetScaler::System::Disk1 
+/check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_threshold_above -I system -F disk1perusage -w 75 -c 80 
+
+NetScaler::HA::Status 
+/check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_string_not -I hanode -F hacurstatus -w YES -c YES 
+
+NetScaler::HA::State 
+./check_netscaler.pl -H  192.168.100.100 -U nsroot -P nsroot -C check_string_not -I hanode -F hacurstate -w UP -c UP 
+
+");
+
+my @args = (
+	{
+		spec    => 'hostname|H=s',
+		usage   => '-H, --hostname=HOSTNAME',
+		desc    => 'Hostname/NSIP of the NetScaler appliance to connect to',
+	},
+	{
+		spec    => 'username|U=s',
+		usage   => '-U, --username=USERNAME',
+		desc    => 'Username to log into box as',
+		default => 'nsroot',
+	},
+	{
+		spec    => 'password|P=s',
+		usage   => '-P, --password=PASSWORD',
+		desc    => 'Password for login username',
+		default => 'nsroot',
+	},
+	{
+		spec	=> 'command|C=s',
+		usage	=> '-C, --command=COMMAND',
+		desc	=> 'Check to be executed on the appliance',
+	},
+	{
+		spec	=> 'identifier|I=s',
+		usage	=> '-I, --identifier=SUBCOMMAND',
+		desc	=> 'Identifier for command',
+		default => '',
+	},
+	{
+		spec	=> 'filter|F=s',
+		usage	=> '-F, --filter=FILTER',
+		desc	=> 'Filter for current command (might be a object name)',
+		default => '',
+	},
+	{
+		spec	=> 'warning|w=s',
+		usage	=> '-w, --warning=INTEGER',
+		desc	=> 'Value for warning',
+	},
+	{
+		spec	=> 'critical|c=s',
+		usage	=> '-c, --critical=INTEGER',
+		desc	=> 'Value for critical',
+	},
+);
+
+foreach my $arg (@args) {
+	add_arg($plugin, $arg);
+}
+
+$plugin->getopts;
+ 
+my $session = Nitro::_login($plugin->opts->hostname, $plugin->opts->username, $plugin->opts->password);
+
+if ($session->{errorcode} != 0 || !($session->{sessionid})) {
+	$plugin->die("ERROR: " . $session->{message});
+}
+
+if ($plugin->opts->command eq 'check_vserver') {
+	check_vserver();
+} elsif ($plugin->opts->command eq 'check_threshold_above') {
+	check_threshold_above();
+} elsif ($plugin->opts->command eq 'check_threshold_below') {
+	check_threshold_below();
+} elsif ($plugin->opts->command eq 'check_string') {
+	check_string();
+} elsif ($plugin->opts->command eq 'check_string_not') {
+	check_string_not();
+} elsif ($plugin->opts->command eq 'dump_stats') {
+	dump_stats();
+} elsif ($plugin->opts->command eq 'dump_vserver') {
+	dump_vserver();
+} else {
+	$plugin->nagios_die('unkown argument for parameter -C (command)', CRITICAL);
+}
+
+my $result = Nitro::_logout($session);
+
+sub add_arg
+{
+	my $plugin = shift;
+	my $arg    = shift;
+
+	my $spec = $arg->{'spec'};
+	my $help = $arg->{'usage'};
+
+	if (defined $arg->{'desc'}) {
+		my @desc;
+
+		if (ref($arg->{'desc'})) {
+			@desc = @{$arg->{'desc'}};
+		}
+		else {
+			@desc = ( $arg->{'desc'} );
+		}
+
+		foreach my $d (@desc) {
+			$help .= "\n   $d";
+		}
+
+		if (defined $arg->{'default'}) {
+			$help .= " (default: $arg->{'default'})";
+		}
+	}
+	elsif (defined $arg->{'default'}) {
+		$help .= "\n   (default: $arg->{'default'})";
+	}
+
+	$plugin->add_arg(
+		spec => $spec,
+		help => $help,
+	);
+}
+
+sub check_vserver
+{
+	my $state = Nitro::_get_stats($session, $plugin->opts->identifier, $plugin->opts->filter);
+
+	my $state_up     = '';
+	my $state_down   = '';
+	my $state_unkown = '';
+	my $state_oos    = '';
+
+	my $counter_up     = 0;
+	my $counter_down   = 0;
+	my $counter_unkown = 0;
+	my $counter_oos    = 0;
+	
+	if ($state->{errorcode} != 0) {
+		$plugin->nagios_die($state->{message}, CRITICAL);
+	}
+
+	if ($plugin->opts->verbose) {
+		print Dumper($state);
+	}
+	$state = $state->{$plugin->opts->identifier};
+	foreach my $state (@{$state}) {
+		# NetScaler API Bug: returns "ENABLED" instead of "UP" when requesting services/servicegroups
+		if ($state->{state} eq "UP" || $state->{state} eq "ENABLED") {
+			$counter_up++;
+		}
+		elsif ($state->{state} eq "DOWN") {
+			$counter_down++;
+			$plugin->add_message(CRITICAL, $state->{name} . " DOWN;");
+		}
+		elsif ($state->{state} eq "OUT OF SERVICE") {
+			$counter_oos++;
+			$plugin->add_message(CRITICAL, $state->{name} . " OUT OF SERVICE;");
+		}
+		elsif ($state->{state} eq "UNKOWN") {
+			$counter_unkown++;
+			$plugin->add_message(CRITICAL, $state->{name} . " UNKOWN;");				
+		} else {
+			$counter_unkown++;
+			$plugin->add_message(CRITICAL, $state->{name} . " UNKOWN;");				
+		}
+			
+		my ($code, $message) = $plugin->check_messages;
+		
+		my $stats = "[" . $counter_up . " UP, " . 
+		                  $counter_down . " DOWN, " .
+				  $counter_oos . " OOS, " .
+				  $counter_unkown . " UNKOWN" . "]";
+				
+		$plugin->nagios_exit($code, $message . " " . $stats);
+	}
+}
+
+sub check_string
+{
+        if (!defined $plugin->opts->filter) {
+                $plugin->nagios_die('command requires parameter for filter', CRITICAL);
+        }
+
+        if (!defined $plugin->opts->warning || !defined $plugin->opts->critical) {
+                $plugin->nagios_die('command requires parameter for warning and critical', CRITICAL);
+        }
+
+	my $values = Nitro::_get_stats($session, $plugin->opts->identifier);
+
+	if ($values->{errorcode} != 0) {
+		$plugin->nagios_die($values->{message}, CRITICAL);
+	}
+
+	if ($plugin->opts->verbose) {
+		print Dumper($plugin->opts->warning);
+		print Dumper($plugin->opts->critical);
+		print Dumper($values);
+	}
+
+	$values = $values->{$plugin->opts->identifier};
+
+        if ($values->{$plugin->opts->filter} eq $plugin->opts->critical) {
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " matches keyword [current: " . $values->{$plugin->opts->filter} . "; critical: " . $plugin->opts->critical . "]", CRITICAL);
+        } elsif ($values->{$plugin->opts->filter} eq $plugin->opts->warning) {
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " matches keyword [current: " . $values->{$plugin->opts->filter} . "; warning: " . $plugin->opts->warning . "]", WARNING);
+        } else {
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " [".$values->{$plugin->opts->filter}."]", OK);
+        }
+}
+
+sub check_string_not
+{
+        if (!defined $plugin->opts->filter) {
+                $plugin->nagios_die('command requires parameter for filter', CRITICAL);
+        }
+
+        if (!defined $plugin->opts->warning || !defined $plugin->opts->critical) {
+                $plugin->nagios_die('command requires parameter for warning and critical', CRITICAL);
+        }
+
+        my $values = Nitro::_get_stats($session, $plugin->opts->identifier);
+
+        if ($values->{errorcode} != 0) {
+                $plugin->nagios_die($values->{message}, CRITICAL);
+        }
+
+        if ($plugin->opts->verbose) {
+                print Dumper($plugin->opts->warning);
+                print Dumper($plugin->opts->critical);
+                print Dumper($values);
+        }
+
+        $values = $values->{$plugin->opts->identifier};
+
+        if ($values->{$plugin->opts->filter} ne $plugin->opts->critical) {
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " not matches keyword [current: " . $values->{$plugin->opts->filter} . "; critical: " . $plugin->opts->critical . "]", CRITICAL);
+        } elsif ($values->{$plugin->opts->filter} ne $plugin->opts->warning) { 
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " not matches keyword [current: " . $values->{$plugin->opts->filter} . "; warning: " . $plugin->opts->warning . "]", WARNING);
+        } else {
+                $plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " [".$values->{$plugin->opts->filter}."]", OK);
+        }
+}
+
+sub check_threshold_above
+{
+	if (!defined $plugin->opts->filter) {
+		$plugin->nagios_die('command requires parameter for filter', CRITICAL);
+	}
+
+	if (!defined $plugin->opts->warning || !defined $plugin->opts->critical) {
+		$plugin->nagios_die('command requires parameter for warning and critical', CRITICAL);
+	}
+
+	my $values = Nitro::_get_stats($session, $plugin->opts->identifier);
+	if ($values->{errorcode} != 0) {
+		$plugin->nagios_die($values->{message}, CRITICAL);
+	}
+
+	$values = $values->{$plugin->opts->identifier};
+
+	if ($plugin->opts->verbose) {
+		print Dumper($plugin->opts->warning);
+		print Dumper($plugin->opts->critical);
+		print Dumper($values);
+	}
+
+	if ($values->{$plugin->opts->filter} >= $plugin->opts->critical) {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " is above threshold [current: " . $values->{$plugin->opts->filter} . "; critical: " . $plugin->opts->critical . "]", CRITICAL);
+	} elsif ($values->{$plugin->opts->filter} >= $plugin->opts->warning) {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " is above threshold [current: " . $values->{$plugin->opts->filter} . "; warning: " . $plugin->opts->warning . "]", WARNING);
+	} else {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " [".$values->{$plugin->opts->filter}."]", OK);
+	}
+}
+
+sub check_threshold_below
+{
+	if (!defined $plugin->opts->filter) {
+		$plugin->nagios_die('command requires parameter for filter', CRITICAL);
+	}
+
+	if (!defined $plugin->opts->warning || !defined $plugin->opts->critical) {
+		$plugin->nagios_die('command requires parameter for warning and critical', CRITICAL);
+	}
+
+	my $values = Nitro::_get_stats($session, $plugin->opts->identifier);
+	if ($values->{errorcode} != 0) {
+		$plugin->nagios_die($values->{message}, CRITICAL);
+	}
+
+	$values = $values->{$plugin->opts->identifier};
+
+	if ($plugin->opts->verbose) {
+		print Dumper($plugin->opts->warning);
+		print Dumper($plugin->opts->critical);
+		print Dumper($values);
+	}
+
+	if ($values->{$plugin->opts->filter} <= $plugin->opts->critical) {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " is below threshold [current: " . $values->{$plugin->opts->filter} . "; critical: " . $plugin->opts->critical . "]", CRITICAL);
+	} elsif ($values->{$plugin->opts->filter} <= $plugin->opts->warning) {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " is below threshold [current: " . $values->{$plugin->opts->filter} . "; warning: " . $plugin->opts->warning . "]", WARNING);
+	} else {
+		$plugin->nagios_die("NetScaler " . $plugin->opts->identifier . "::" . $plugin->opts->filter . " [".$values->{$plugin->opts->filter}."]", OK);
+	}
+}
+
+sub dump_stats
+{
+	my $values = Nitro::_get_stats($session, $plugin->opts->identifier, $plugin->opts->filter);
+        if ($values->{errorcode} != 0) {
+                $plugin->nagios_die($values->{message}, CRITICAL);
+        }
+	print Dumper($values);
+}
+
+sub dump_vserver
+{
+	my $values = Nitro::_get_stats($session, $plugin->opts->identifier, $plugin->opts->filter);
+        if ($values->{errorcode} != 0) {
+                $plugin->nagios_die($values->{message}, CRITICAL);
+        }
+        print Dumper($values);
+}
