4eb8d2df6d2fddd4d40cb587dbd8e6d2c31c2553
Simon Lauger <simon@lauger.name>
2017-08-23 00:27:43 +0200

    enabled verbose output in travis_test.sh

--- tests/travis_test.sh
+++ tests/travis_test.sh
@@ -17,31 +17,31 @@ sshpass -p nsroot scp tests/ns.conf nsroot@${CIP}:/home/nsroot/ns.conf
 sshpass -p nsroot ssh nsroot@${CIP} "/var/netscaler/bins/cli_script.sh /home/nsroot/ns.conf"
 
 # do some basic plugin tests
-./check_netscaler.pl -H ${CIP} -C sslcert
-./check_netscaler.pl -H ${CIP} -C interfaces
-./check_netscaler.pl -H ${CIP} -C nsconfig
-./check_netscaler.pl -H ${CIP} -C hastatus
+./check_netscaler.pl -v -H ${CIP} -C sslcert
+./check_netscaler.pl -v -H ${CIP} -C interfaces
+./check_netscaler.pl -v -H ${CIP} -C nsconfig
+./check_netscaler.pl -v -H ${CIP} -C hastatus
 
 # fails on vpx instances
-#./check_netscaler.pl -H ${CIP} -C hwinfo
+#./check_netscaler.pl -v -H ${CIP} -C hwinfo
 
-./check_netscaler.pl -H ${CIP} -s -C above -o system -n memusagepcnt -w 75 -c 80
-./check_netscaler.pl -H ${CIP} -s -C above -o system -n cpuusagepcnt,mgmtcpuusagepcnt -w 75 -c 80
-./check_netscaler.pl -H ${CIP} -s -C above -o system -n disk0perusage,disk1perusage -w 75 -c 80
+./check_netscaler.pl -v -H ${CIP} -s -C above -o system -n memusagepcnt -w 75 -c 80
+./check_netscaler.pl -v -H ${CIP} -s -C above -o system -n cpuusagepcnt,mgmtcpuusagepcnt -w 75 -c 80
+./check_netscaler.pl -v -H ${CIP} -s -C above -o system -n disk0perusage,disk1perusage -w 75 -c 80
 
 # test state all objects at once
-./check_netscaler.pl -H ${CIP} -C state -o lbvserver
-./check_netscaler.pl -H ${CIP} -C state -o csvserver
-./check_netscaler.pl -H ${CIP} -C state -o service
-./check_netscaler.pl -H ${CIP} -C state -o servicegroup
-./check_netscaler.pl -H ${CIP} -C state -o server
+./check_netscaler.pl -v -H ${CIP} -C state -o lbvserver
+./check_netscaler.pl -v -H ${CIP} -C state -o csvserver
+./check_netscaler.pl -v -H ${CIP} -C state -o service
+./check_netscaler.pl -v -H ${CIP} -C state -o servicegroup
+./check_netscaler.pl -v -H ${CIP} -C state -o server
 
 # test state of single objects
-./check_netscaler.pl -H ${CIP} -C state -o lbvserver -n vs_lb_http_web1
-./check_netscaler.pl -H ${CIP} -C state -o csvserver -n vs_cs_http_web1
-./check_netscaler.pl -H ${CIP} -C state -o service -n svc_http_web1
-./check_netscaler.pl -H ${CIP} -C state -o servicegroup -n sg_http_web1
-./check_netscaler.pl -H ${CIP} -C state -o server -n srv_web1
+./check_netscaler.pl -v -H ${CIP} -C state -o lbvserver -n vs_lb_http_web1
+./check_netscaler.pl -v -H ${CIP} -C state -o csvserver -n vs_cs_http_web1
+./check_netscaler.pl -v -H ${CIP} -C state -o service -n svc_http_web1
+./check_netscaler.pl -v -H ${CIP} -C state -o servicegroup -n sg_http_web1
+./check_netscaler.pl -v -H ${CIP} -C state -o server -n srv_web1
 
 # Everything OK
 exit 0
